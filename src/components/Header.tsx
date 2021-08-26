3b7e8b4c709f5496ee1a464b5105d5d7aa7768c0
ybw903 <conner903@naver.com>
2021-08-26 12:37:24 +0900

    style: Create header modue css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,29 @@
+import { useState } from 'react';
+import styles from '../../styles/Header.module.css';
 export default function Header() {
+
+    const [isMenuOpen,setIsMenuOpen] = useState<boolean>(false);
+
+    const menuClickEventHandler = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
 
     return(
         <header>
-            <div>
-                <h1>여기저기로</h1>
-                <nav>
-                    <a>여행지 둘러보기</a>
-                    <a>여행 일정 등록</a>
-                    <a>여행 후기</a>
-                    <a>MyProfile</a>
-                </nav>
-            </div>
+            <nav className={styles.navbar}>
+                <div className={styles.navbar__title}>
+                    <h1>여기저기로</h1>
+                </div>
+                <ul className= {!isMenuOpen? styles.navbar__menu : `${styles.navbar__menu} ${styles.active}`}>
+                    <li><a>여행지 둘러보기</a></li>
+                    <li><a>여행 일정 등록</a></li>
+                    <li><a>여행 후기</a></li>
+                    <li><a>MyProfile</a></li>
+                </ul>
+                <div className={styles.navbar__toggleBtn} onClick={menuClickEventHandler}>
+                    햄버거
+                </div>
+            </nav>
         </header>
     );
 }
