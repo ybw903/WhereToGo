ce0ba1a7e5f94a4aa33e530f3541cef0f6974a56
ybw903 <conner903@naver.com>
2021-08-26 09:57:57 +0900

    feat: Create header component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Header from '../src/components/Header'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <div>
+      <Header/>
+      <Component {...pageProps} />
+    </div>
+  ) 
 }
 export default MyApp
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+export default function Header() {
+
+    return(
+        <header>
+            <div>
+                <h1>여기저기로</h1>
+                <nav>
+                    <a>여행지 둘러보기</a>
+                    <a>여행 일정 등록</a>
+                    <a>여행 후기</a>
+                    <a>MyProfile</a>
+                </nav>
+            </div>
+        </header>
+    );
+}
