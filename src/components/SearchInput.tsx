50c4cd8fd5b89ae46241ef9969d210d19997b480
ybw903 <conner903@naver.com>
2021-08-26 16:35:43 +0900

    feat: Create searchinput component

---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,13 @@
+export default function SearchInput() {
+
+
+
+    return (
+        <div>
+            <div>
+                <div></div>
+                <div>검색어를 입력해주세요.</div>
+            </div>
+        </div>
+    )
+}
