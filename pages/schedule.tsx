68de3269e86398c72b43b80e6abf1efee7490dc8
ybw903 <conner903@naver.com>
2021-08-26 15:07:50 +0900

    feat: create schedule page

---
+++ pages/schedule.tsx
@@ -0,0 +1,17 @@
+import TourCalender from "../src/components/TourCalender";
+
+export default function Schedule() {
+    return (
+        <div>
+            <div>
+                <h1>여행 달력</h1>
+                <select>
+                    <option>월 간격으로 보기</option>
+                    <option>2주 간격으로 보기</option>
+                    <option>1주 간격으로 보기</option>
+                </select>
+            </div>
+            <TourCalender></TourCalender>
+        </div>
+    )
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styles from '../../styles/Header.module.css';
+import Link from 'next/link';
 export default function Header() {
 
     const [isMenuOpen,setIsMenuOpen] = useState<boolean>(false);
@@ -16,7 +17,9 @@ export default function Header() {
                 </div>
                 <ul className= {!isMenuOpen? styles.navbar__menu : `${styles.navbar__menu} ${styles.active}`}>
                     <li><a>여행지 둘러보기</a></li>
-                    <li><a>여행 일정 등록</a></li>
+                    <li>
+                        <Link href="schedule"><a>여행 일정 등록</a></Link>
+                    </li>
                     <li><a>여행 후기</a></li>
                     <li><a>MyProfile</a></li>
                 </ul>
