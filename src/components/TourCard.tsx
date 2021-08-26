e38a3badcf1869fc5863bd275bd5b93861663e5a
ybw903 <conner903@naver.com>
2021-08-26 10:02:02 +0900

    feat: Create tour card component

---
+++ src/components/TourCard.tsx
@@ -0,0 +1,8 @@
+export default function TourCard() {
+    return (
+        <div className="tour-card">
+            <div className="tour-title"></div>
+            <div className="tour-content"></div>
+        </div>
+    );
+}
