9df976b2a79121f895dd58c2b801517624f1404c
ybw903 <conner903@naver.com>
2021-08-27 20:36:28 +0900

    feat: review page render

--- pages/reviews.tsx
+++ pages/reviews.tsx
@@ -1,7 +1,25 @@
+import SearchInput from "../src/components/SearchInput";
+
 export default function reviews() {
+
+    const reviewItems = [{"title" : "제목", "content" : "내용", "imgUrl": "imgUrl"}]    
+
     return(
         <div>
-            Reviews;
+            <SearchInput/>
+            {reviewItems.map(revieItem =>
+                    <div>
+                        <div>
+                            <img src={revieItem.imgUrl}></img>
+                        </div>
+                        <div>
+                            <div>{revieItem.title}</div>
+                            <div>{revieItem.content}</div>
+                        </div>
+                    </div>
+
+            )}
+
         </div>
     )
 }
